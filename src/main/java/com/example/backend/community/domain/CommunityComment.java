f2d158a93746b9a35d366ff1dff1bc9789db1fe7
somsom13 <somin1113@naver.com>
2022-04-11 22:55:39 +0900

    :sparkles:add: 잡담 게시판 댓글 엔티티 생성 (#3)

---
+++ src/main/java/com/example/backend/community/domain/CommunityComment.java
@@ -0,0 +1,38 @@
+package com.example.backend.community.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "communityComments")
+public class CommunityComment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //mysql은 pk값 자동증가로 IDENTITY 를 사용한다
+    @Column(name = "communityCommentId")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean isParentComment; //true 면 원댓글 (부모댓글) 이라는 의미
+
+    @Column(nullable = false)
+    private Long commentGroupId;
+    //원댓글과 그 대댓글들의 묶음이 몇 번 그룹인지에 대한 내용
+
+    @Column(nullable = false)
+    private LocalDateTime createdTime;
+
+    //하나의 게시글에 여러 댓글 -> 게시글:댓글 = 1:M
+    //댓글이 M 이므로 댓글 엔티티에서 ManyToOne 사용, comment 엔티티가 연관관계의 주인
+    @ManyToOne(targetEntity = CommunityBoard.class)
+    @JoinColumn(name = "communityBoardId")
+    private CommunityBoard communityBoard;
+
+    //manyToOne User 연관관계 매핑 추가 필요
+}
