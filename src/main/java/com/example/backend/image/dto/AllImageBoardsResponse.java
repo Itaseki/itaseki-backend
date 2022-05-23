6b26352e8f74574eb66d88a1368a827b40d6b839
kitaee <dlrlxo999@naver.com>
2022-05-23 18:41:38 +0900

    ✨ [FEAT] : implement ImageBoard API(1)

---
+++ src/main/java/com/example/backend/image/dto/AllImageBoardsResponse.java
@@ -0,0 +1,30 @@
+package com.example.backend.image.dto;
+
+import com.example.backend.community.domain.CommunityBoard;
+import com.example.backend.community.dto.AllCommunityBoardsResponse;
+import com.example.backend.image.domain.ImageBoard;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class AllImageBoardsResponse {
+    private Long id;
+    private String imageBoardTitle;
+    private String writerNickname;
+    private LocalDateTime createdTime;
+    private Integer viewCount;
+    private Integer likeCount;
+
+    public static AllImageBoardsResponse fromEntity(ImageBoard imageBoard){
+        return AllImageBoardsResponse.builder()
+                .id(imageBoard.getId()).imageBoardTitle(imageBoard.getImageBoardTitle()).createdTime(imageBoard.getCreatedTime())
+                .likeCount(imageBoard.getLikeCount()).viewCount(imageBoard.getViewCount())
+                .writerNickname(imageBoard.getUser().getNickname())
+                .build();
+    }
+}
--- src/main/java/com/example/backend/image/service/ImageBoardService.java
+++ src/main/java/com/example/backend/image/service/ImageBoardService.java
@@ -2,15 +2,24 @@ package com.example.backend.image.service;
 
 import com.example.backend.community.domain.CommunityBoard;
 import com.example.backend.community.domain.CommunityBoardImage;
+import com.example.backend.community.dto.AllBoardResponseWithPageCount;
 import com.example.backend.image.domain.ImageBoard;
+import com.example.backend.image.dto.AllImageBoardsResponse;
+import com.example.backend.image.dto.AllImageResponseWithPageCount;
+import com.example.backend.image.dto.DetailImageBoardResponse;
 import com.example.backend.image.repository.ImageBoardRepository;
 import com.example.backend.s3Image.AwsS3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -19,10 +28,56 @@ public class ImageBoardService {
     private final ImageBoardRepository imageBoardRepository;
     private final AwsS3Service awsS3Service;
 
-    public void savePost(ImageBoard imageBoard, List<MultipartFile> files){
+    public void savePost(ImageBoard imageBoard){
         imageBoardRepository.save(imageBoard);
     }
 
+    public void deleteImageBoard(ImageBoard imageBoard){
+        imageBoard.setStatus(false);
+        imageBoardRepository.save(imageBoard);
+    }
 
+    public ImageBoard findImageBoardEntity(Long imageBoardId){
+        Optional<ImageBoard> imageBoard = imageBoardRepository.findById(imageBoardId);
+        if(imageBoard.isPresent()&&imageBoard.get().getStatus()){
+            return imageBoard.get();
+        }
+        return null;
+    }
+
+    public void updateImageBoardViewCount(ImageBoard imageBoard){
+        imageBoard.updateViewCount();
+        imageBoardRepository.save(imageBoard);
+    }
+
+    public List<AllImageBoardsResponse> getBestResponseOfImageBoard(){
+        return toAllImageBoardResponse(imageBoardRepository.findBestBoards());
+    }
+
+    private List<AllImageBoardsResponse> toAllImageBoardResponse(List<ImageBoard> imageBoards){
+        return imageBoards.stream()
+                .filter(imageBoard -> imageBoard.getStatus().equals(true))
+                .map(imageBoard -> AllImageBoardsResponse.fromEntity(imageBoard))
+                .collect(Collectors.toList());
+    }
+
+    public String getImageUrlInPost(ImageBoard imageBoard){
+        return imageBoard.getImageUrl();
+    }
+
+    public DetailImageBoardResponse getDetailImageResponse(ImageBoard imageBoard, Long loginId){
+        Long imageWriterId = imageBoard.getUser().getUserId();
+        return DetailImageBoardResponse.fromEntity(imageBoard, loginId);
+    }
+
+    public AllImageResponseWithPageCount getAllResponseOfImageBoard(Pageable pageable, String query){
+        String[] queryList = null;
+        if(query!=null){
+            queryList = query.split(" ");
+        }
+        Page<ImageBoard> imageBoardPage = imageBoardRepository.findAll(pageable,queryList);
+        List<AllImageBoardsResponse> imageBoardsResponses = toAllImageBoardResponse(imageBoardPage.getContent());
+        return new AllImageResponseWithPageCount(imageBoardPage.getTotalPages(),imageBoardsResponses);
+    }
 
 }
