2d6318ff7718b98dbce09f280e470509274932ab
somin <70891072+somsom13@users.noreply.github.com>
2022-06-30 10:30:42 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd $REPOSITORY
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep  -f $APP_NAME)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -15,15 +15,15 @@ then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME # /home/ubuntu/app/build/libs/demo.xxxx.jar
+JAR_NAME=$(ls $REPOSITORY | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
 
 echo "> 파일명: $JAR_NAME"
-echo "> 파일경로: $JAR_NAME"
+echo "> 파일경로: $JAR_PATH"
 
 echo "> 새 애플리케이션 배포"
 
@@ -33,4 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/applications.properties,file:///home/ubuntu/backend-enc/application-oauth.properties, file:///home/ubuntu/backend-enc/application-mysql.properties -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
