3277765e70832c65630aef0fffb5c28f1391402a
somsom13 <somin1113@naver.com>
2022-07-04 22:05:30 +0900

    🐛fix: 전체 영상 조회 - 페이지수 수정 (#55)

--- src/main/java/com/example/backend/video/repository/CustomVideoRepository.java
+++ src/main/java/com/example/backend/video/repository/CustomVideoRepository.java
@@ -1,13 +1,14 @@
 package com.example.backend.video.repository;
 
 import com.example.backend.video.domain.Video;
+import com.example.backend.video.dto.TempVideoDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CustomVideoRepository {
-    Page<Video> findAll(Pageable pageable, List<String> tags, String nickname, List<String> queries);
+    TempVideoDto findAll(Pageable pageable, List<String> tags, String nickname, List<String> queries);
     List<Video> findBestVideos();
     List<Video> findTitleLike(String searchTitle, String order);
 }
