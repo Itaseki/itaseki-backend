584da9754da5f1d738d868e2902e962e6d5c21f7
kitaee <dlrlxo999@naver.com>
2022-07-28 00:23:11 +0900

    [MOD] : chat API

--- src/main/java/com/example/backend/chat/ChatMessageDTO.java
+++ src/main/java/com/example/backend/chat/ChatMessageDTO.java
@@ -10,4 +10,5 @@ public class ChatMessageDTO {
     private String roomId;
     private String writer;
     private String message;
+    private String profileUrl = "http://k.kakaocdn.net/dn/uJ3nC/btrG5qJDRzA/Y8aFVKvPsaIRLsdH1x203K/img_640x640.jpg";
 }
