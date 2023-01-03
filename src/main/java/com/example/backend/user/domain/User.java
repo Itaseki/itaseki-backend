d1333a288f7a64e0fe0181d3944affb453e8b101
kitaee <dlrlxo999@naver.com>
2023-01-03 17:42:18 +0900

    🔨 [MOD] : userId 컬럼값 수정

--- src/main/java/com/example/backend/user/domain/User.java
+++ src/main/java/com/example/backend/user/domain/User.java
@@ -27,7 +27,7 @@ public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user")
+    @Column(name = "user_id")
     private Long userId;
 
     @Column(nullable = false)
