973cdf2bc5324bad4d73a1104c387ed93212d342
kitaee <dlrlxo999@naver.com>
2023-01-13 20:15:57 +0900

    [MOD] : CORS 설정 변경

--- src/main/java/com/example/backend/security/WebMvcConfig.java
+++ src/main/java/com/example/backend/security/WebMvcConfig.java
@@ -2,9 +2,11 @@ package com.example.backend.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
@@ -13,9 +15,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedHeaders("*")
-                .allowedMethods("*")
-                .exposedHeaders("ITASEKKI")
+                .allowedOrigins("http://localhost:3000")
                 .allowCredentials(true)
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
+                .exposedHeaders("ITASEKKI")
                 .maxAge(3000);
     }
 }
