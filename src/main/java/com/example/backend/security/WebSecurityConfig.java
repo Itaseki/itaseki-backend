cf4776c59bd8e3bbace962ceaf9ac98a50f80c01
kitaee <dlrlxo999@naver.com>
2023-01-31 22:00:10 +0900

    URL 변경

--- src/main/java/com/example/backend/security/WebSecurityConfig.java
+++ src/main/java/com/example/backend/security/WebSecurityConfig.java
@@ -64,6 +64,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.httpBasic().disable()
                 .authorizeRequests()// 요청에 대한 사용권한 체크
+                .antMatchers(HttpMethod.POST).authenticated()
+                .antMatchers(HttpMethod.GET).permitAll()
                 .antMatchers(AUTHENTICATED_URL_ARRAY).authenticated()
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
                 .anyRequest().denyAll()
