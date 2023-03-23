5de24eeb572dc4a10d380fdd3195e7bbaf1f6f28
somsom13 <somin1113@naver.com>
2023-03-23 20:55:41 +0900

    fix: 인증 오류 URL 임시 추가

--- src/main/java/com/example/backend/security/WebSecurityConfig.java
+++ src/main/java/com/example/backend/security/WebSecurityConfig.java
@@ -65,6 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().disable()
                 .authorizeRequests()// 요청에 대한 사용권한 체크
                 .antMatchers(HttpMethod.POST).authenticated()
+                .antMatchers(HttpMethod.GET, "/boards/video/info/{userId}").authenticated() // authenticated 필터링이 안되길래 임시 추가
                 .antMatchers(HttpMethod.GET).permitAll()
                 .antMatchers(AUTHENTICATED_URL_ARRAY).authenticated()
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
